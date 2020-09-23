8ddd01fef50b9974831601d75b3a714c9d067531
George Soderholm <georgesoderholm@gmail.com>
2020-09-23 09:55:23 -0600

    Add method to get an array of all ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -35,6 +35,30 @@ class RecipeTest < Minitest::Test
 
     assert_equal expected, recipe1.ingredients_required
   end
+  def test_it_can_add_multiple_ingredients_at_different_times_and_maintain_correct_numbers
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
 
-  
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    expected = { ingredient1 => 6, ingredient2 => 8 }
+
+    assert_equal expected, recipe1.ingredients_required
+  end
+
+  def test_it_can_check_the_ingredients_it_has
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    expected = [ingredient1, ingredient2]
+    assert_equal expected, recipe1.ingredients
+  end
 end
