1988c0c8ad9d812b7b07d17b0c69864d833fefe5
George Soderholm <georgesoderholm@gmail.com>
2020-09-23 09:35:43 -0600

    Add two methods to be able to check the stock and to restock

---
+++ lib/pantry.rb
@@ -0,0 +1,19 @@
+class Pantry
+  attr_reader :stock
+  def initialize
+    @stock = Hash.new(0)
+  end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
+
+  end
+
+  def restock(ingredient, quantity)
+    if @stock[ingredient].nil?
+      @stock[ingredient] = quantity
+    else
+      @stock[ingredient] += quantity
+    end
+  end
+end
