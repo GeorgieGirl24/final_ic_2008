930b21885cc5bddb378035b60ec9721d9d81c703
George Soderholm <georgesoderholm@gmail.com>
2020-09-23 09:37:10 -0600

    Add method to add ingredients to recipe

---
+++ lib/recipe.rb
@@ -0,0 +1,15 @@
+class Recipe
+  attr_reader :name, :ingredients_required
+  def initialize(name)
+    @name = name
+    @ingredients_required = {}
+  end
+
+  def add_ingredient(ingredient, quantity)
+    if @ingredients_required[ingredient].nil?
+      @ingredients_required[ingredient] = quantity
+    else
+      @ingredients_required[ingredient] += quantity
+    end 
+  end
+end
